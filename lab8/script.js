693c6d5ecc00304f9dba1ce4197ab3343826afd7
ortamfag <a_zhidov@outlook.com>
2021-05-30 18:47:14 +0300

    ADD: 8 lab animation&icon

--- lab8/script.js
+++ lab8/script.js
@@ -1,2 +1,10 @@
-const burger = document.querySelector('#menu-burger');
-burger.addEventListener('click', () => burger.classList.toggle('active'));
+let burger = document.getElementById('burger');
+line1 = document.getElementById('line1');
+line2 = document.getElementById('line2');
+line3 = document.getElementById('line3');
+
+burger.addEventListener('click', function(){
+    line1.classList.toggle('line1-animate');
+    line2.classList.toggle('line2-animate');
+    line3.classList.toggle('line3-animate');
+});
